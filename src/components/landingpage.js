794c61aa5e5203d0ee4f4d24172e650b7db80dad
Elżbieta Szulc <elzbieta.szulc@monterail.com>
2020-02-25 22:20:25 +0100

    Add links to Readme file + add description of aplication

---
+++ src/components/landingpage.js
@@ -0,0 +1,12 @@
+import React, { Compoentnt } from 'react';
+
+class Landing extends Components {
+    render() {
+        return (
+            <div style={{ width: '100%', margin: 'auto' }}>
+
+            </div>
+        )
+    }
+}
+export default Landing;
