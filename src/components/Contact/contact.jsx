f16b07a76b90aa87a7898d1920d692465bfd7602
Elzbieta Szulc <elszulc@wp.pl>
2020-04-02 23:18:36 +0200

    Add style to mission and contact me pages

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -4,11 +4,10 @@ import { Grid } from 'react-mdl';
 class Contact extends Component {
     render() {
       return(
-        <div className="contact-body">
-          <Grid className="contact-grid">
-              <h1>Contact me!</h1>
-          </Grid>
-        </div>
+        <Grid className="contact-grid">
+            <h1>Contact me</h1>
+            <h3>If you would like to help me develop the application, just contact me via the email address: elszulc@wp.pl</h3>
+        </Grid>
       )
     }
   }
--- src/components/MissionPage/mission.jsx
+++ src/components/MissionPage/mission.jsx
@@ -7,6 +7,10 @@ class Mission extends Component {
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="mission-grid">
                     <h1>Our Mission</h1>
+                    <h3>Our mission is to promote the right balance between work and personal life. 
+                    Believers that this is possible thanks to appropriate organization. 
+                    The personal growth app will help you manage your time and responsibilities. 
+                    If you want to take care of your personal development and achieve your goals - you should use the personal growth app!</h3>
                 </Grid>
             </div>
         )
