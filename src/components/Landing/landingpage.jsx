79ff725543c1ec0aaaf9176219c0f1f0406f4e68
Elzbieta Szulc <elszulc@wp.pl>
2020-04-10 22:54:05 +0200

    Add styled components to landing page

--- src/components/Landing/landingpage.jsx
+++ src/components/Landing/landingpage.jsx
@@ -1,8 +1,12 @@
 /*WIP*/
 import React, { Component } from 'react';
 import { Grid } from 'react-mdl';
-import styled from 'styled-components';
+import styled, {createGlobalStyle} from 'styled-components';
+import importGoogleFonts from 'import-google-fonts'
 
+const GoogleFonts = importGoogleFonts(createGlobalStyle, null, [
+    'Sawarabi Mincho'
+  ])
 
 const StyledDiv = styled.div`
     background: #FEAC5E;  /* fallback for old browsers */
@@ -17,15 +21,25 @@ const StyledDiv = styled.div`
     height: 100%;
 `
 const StyledTitle = styled.h1`
-    font-size: 3rem;
-    padding: 4rem;
+    font-size: 100px;
+    padding: 5rem;
     text-align: center;
-    font-family: Sawarabi Mincho, sans-serif;
+    font-family: 'Sawarabi Mincho', sans-serif;
     display: inline-block;
     position: relative;
     margin-top: 4rem;
     margin-bottom: 2rem;
     color:rgb(255, 255, 255);
+`
+
+const StyledSubtitle = styled.h2`
+    font-size: 5rem;
+    text-align: center;
+    font-family: 'Sawarabi Mincho', 'sans-serif';
+    display: inline-block;
+    position: relative;
+    margin-bottom: 2rem;
+    color: rgb(255, 255, 255);
 `
 
 class Landing extends Component {
@@ -35,8 +49,10 @@ class Landing extends Component {
                     <div className="header__text-box">
                         <StyledTitle>
                             <h1>Check how to measure your personal growth!</h1>
-                        </StyledTitle>
-                        <h2>Our aplication will help you change our life and control your habits.</h2>
+                        </StyledTitle><br />
+                        <StyledSubtitle>
+                            <h2>Our aplication will help you change our life and control your habits.</h2>
+                        </StyledSubtitle>
                         <br />
                         <a href="login" className="btn btn--white btn---animated">Discover our app</a>
                     </div>
