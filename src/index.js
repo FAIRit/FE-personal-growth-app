f4dd4ad11e16d30a5bee21643e77ed2fbf9bd432
Elzbieta Szulc <elszulc@wp.pl>
2020-04-14 08:23:31 +0200

    Add style improvements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './agregators/App';
 import './index.css';
-import registerServiceWorker from './RegisterServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter } from 'react-router-dom';
